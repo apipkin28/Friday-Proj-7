2f2915bd91095a98eb86c980f5133a04c7f43ee0
Abigail Pipkin <abigailpipkinttu@gmail.com>
2024-03-30 11:34:04 -0500

    defined sign-in function

--- signinpage.py
+++ signinpage.py
@@ -2,3 +2,23 @@ import tkinter as tk
 from tkinter import messagebox
 import sqlite3
 
+# defining function to handle sign-in
+def signin():
+    email = entry_email.get()
+    password = entry_password.get()
+
+    # connect to db
+    conn = sqlite3.connect("login.db")
+    cursor = conn.cursor()
+
+    # check for email and password combo in db
+    cursor.execute("SELECT * FROM users WHERE email=? AND password=?", (email, password))
+    user = cursor.fetchone()
+
+    if user:
+        result_label.config(text="Log in successful.", fg="green")
+    else:
+        result_label.config(text="Email or password incorrect.", fg="red")
+
+    conn.close()
+
